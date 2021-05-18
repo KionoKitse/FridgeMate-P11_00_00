ba93296f1b8491ad396b04f48fa5a6b7091e40c2
KionoKitse <57193934+KionoKitse@users.noreply.github.com>
2021-05-18 06:41:53 -0400

    Database backup

---
+++ Main/Test.php
@@ -0,0 +1,12 @@
+<!--Testing PHP generate page -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link href="css/mainstyle.css" rel="stylesheet" type="text/css">
+    <script src="js/mainstyle.js"></script>
+</head>
+<?php
+    include 'php/recipefunc.php';
+?>
