efddd728766ff198d22c437981722368aec3b915
Nishio <l.nishio@servorobot.com>
2021-05-25 17:07:24 -0400

    Update dbconnect.php

--- Main/dbconnect.php
+++ Main/dbconnect.php
@@ -6,7 +6,7 @@
         // Try and connect to the database, if a connection has not been established yet
         if(!isset($connection)) {
             // Load configuration as an array. Use the actual location of your configuration file
-            $config = parse_ini_file('../private/config.ini');
+            $config = parse_ini_file(__DIR__ . '/../private/config.ini');
             $connection = mysqli_connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
         }
         // If connection was not successful, handle the error
